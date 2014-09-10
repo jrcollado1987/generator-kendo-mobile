2d609f091e1365caf3851f63f1a4d0409f251265
Miroslav Miroslavov <miroslav.miroslavov@gmail.com>
2014-09-10 13:59:57 +0300

    Fixing the tests.

--- test/test-app.js
+++ test/test-app.js
@@ -7,21 +7,32 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('kendo-mobile:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
-      .withPrompt({
-        someOption: true
-      })
-      .on('end', done);
-  });
+    before(function (done) {
+        helpers.run(path.join(__dirname, '../app'))
+            .inDir(path.join(os.tmpdir(), './temp-test'))
+            .withOptions({})
+            .withGenerators([path.join(__dirname, '../view')])
+            .withPrompts({
+                view: 'default'
+            })
+            .on('end', done);
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
-  });
+    it('creates files', function () {
+        assert.file([
+            'app/views/default.html',
+            'app/scripts/default.js',
+            'package.json',
+            '.editorconfig',
+            '.jshintrc'
+        ]);
+    });
+
+    it('creates the initial view', function () {
+        assert.file([
+            'app/views/default.html',
+            'app/scripts/default.js'
+        ]);
+    });
+
 });
