8eaa5c5ee5a13b789068a8fe4661b0d675e8a8bf
Miroslav Miroslavov <miroslav.miroslavov@gmail.com>
2014-09-12 10:16:48 +0300

    DataSource generator. SchemaGenerator base.

---
+++ generators/dataSource/index.js
@@ -0,0 +1,24 @@
+'use strict';
+var SchemaGenerator = require('../../schema/schemaGenerator'),
+    _ = require('lodash');
+
+var KendoMobileSignupGenerator = SchemaGenerator.extend({
+    initializing: function () {
+    },
+
+    writing: {
+        app: function () {
+            var that = this;
+        },
+
+        projectfiles: function () {
+            var model = 'app/scripts/signup.js';
+            this.template('model.js', model);
+        }
+    },
+
+    end: function () {
+    }
+});
+
+module.exports = KendoMobileSignupGenerator;
