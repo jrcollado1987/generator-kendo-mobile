46f568a62922173d3ec7fd8b62b0ef111d2c7ac8
Bilyana Ficheva <bilyanaf@gmail.com>
2014-09-15 16:45:52 +0300

    Test commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,6 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['jshint']);
