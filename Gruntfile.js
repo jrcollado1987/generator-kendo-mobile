f9ca9c2650514fb7c28e36603e0179f59f47c9fa
Bilyana Ficheva <bilyanaf@gmail.com>
2014-09-15 16:32:13 +0300

    Add grunt file with jshint

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                reporter: require('jshint-junit-reporter'),
+                reporterOutput: "junit-output.xml",
+                jshintrc: '.jshintrc',
+                ignores: ['client/scripts/require.js']
+            },
+            build: [
+                "generators/**/*.js",
+                "lib/*.js",
+                "*.js"
+            ]
+        },
+        watch: {
+            less: { files: 'client/styles/*.less', tasks: [ 'less' ] }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jshint']);
+};
