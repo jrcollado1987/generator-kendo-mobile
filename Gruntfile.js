485356e3d0f7f505e3888ee232a9e7524c612fc8
Miroslav Miroslavov <miroslav.miroslavov@gmail.com>
2014-09-16 15:14:51 +0300

    Grunt tasks for debuging.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 module.exports = function (grunt) {
 
     // Project configuration.
@@ -25,17 +27,53 @@ module.exports = function (grunt) {
                 files: {
                     src: [
                         //"generators/**/*.js",
-                        "./lib/**/*.js" 
+                        "./lib/**/*.js"
                         //"Gruntfile.js"
                     ]
+                }
+            }
+        },
+        nodemon: {
+            debug: {
+                script: path.join(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE, '\\AppData\\Roaming\\npm\\node_modules\\yo\\cli.js'),
+                options: {
+                    nodeArgs: ['--debug-brk'],
+                    args: ['kendo-mobile'],
+                    ignore: ['generated/**', 'node_modules/**', 'public/**', '*.log', '*.txt'],
+                    env: {
+                        PORT: '3000'
+                    }
+                }
+            }
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true
+            },
+            debug: ['nodemon', 'node-inspector'],
+            debugyo: ['nodemon:debugyo', 'node-inspector']
+        },
+        'node-inspector': {
+            default: {
+                options: {
+                    'web-port': 8088,
+                    'web-host': 'localhost',
+                    'debug-port': 5858,
+                    'save-live-edit': true
                 }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-jscs');
+
+    grunt.loadNpmTasks('grunt-node-inspector');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    grunt.registerTask('debug', ['concurrent:debug']);
+    grunt.registerTask('debugyo', ['concurrent:debugyo']);
 
     grunt.registerTask('default', ['jshint', 'jscs']);
 };
