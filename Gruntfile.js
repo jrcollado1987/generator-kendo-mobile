16f67497e4633f87a9b0b57418b15d6ddb5df7bd
Bilyana Ficheva <bilyanaf@gmail.com>
2014-09-16 13:11:25 +0300

    Add jscs task in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,24 +6,36 @@ module.exports = function (grunt) {
         jshint: {
             options: {
                 reporter: require('jshint-junit-reporter'),
-                reporterOutput: "junit-output.xml",
+                reporterOutput: "output/junit-output-jshint.xml",
                 jshintrc: '.jshintrc',
                 ignores: ['client/scripts/require.js']
             },
             build: [
                 "generators/**/*.js",
-                "lib/*.js",
-                "*.js"
+                "lib/*.js"
             ]
         },
-        watch: {
-            less: { files: 'client/styles/*.less', tasks: [ 'less' ] }
+        jscs: {
+            options: {
+                config: ".jscsrc",
+                reporter: "junit",
+                reporterOutput: "output/junit-output-jscs.xml"
+            },
+            all: {
+                files: {
+                    src: [
+                        //"generators/**/*.js",
+                        "./lib/**/*.js" 
+                        //"Gruntfile.js"
+                    ]
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jscs');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'jscs']);
 };
